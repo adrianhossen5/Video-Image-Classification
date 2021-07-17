70bb8689b681896a075dba922c6765bd67a839f0
adrianhossen5 <86932142+adrianhossen5@users.noreply.github.com>
2021-07-17 11:06:21 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,41 +2,29 @@ const mobilenet;
 const video;
 
 function modelIsReady(){
-
 	console.log("Model is Ready");
 	mobilenet.predict(gotResults);
-
 }
 
 function gotResults(error, results){
-
 	if(error){
-
 		console.error(error);
-
-	} else {
-		
+	} else {	
 		console.log(results);
 		fill(0);
 		createDiv(`Label: ${results[0].label}`);
-		createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
-		
+		createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);		
 	}
-
 }
 
 function setup() {
-
 	createCanvas(500, 480);
 	video = createCapture(VIDEO);
 	video.hide();
 	background(0);
 	mobilenet = ml5.imageClassifier("MobileNet", video, modelIsReady);
-	
 }
 
 function draw(){
-
 	image(video, 0 ,0);
-
 }
